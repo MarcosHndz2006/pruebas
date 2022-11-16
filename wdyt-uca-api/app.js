f041b141c400a4c4c563cc2f17328342c7d8b817
00007518 <00007518@uca.edu.sv>
2022-11-16 09:53:28 -0600

    adding project

--- wdyt-uca-api/app.js
+++ wdyt-uca-api/app.js
@@ -1,18 +1,20 @@
-var express = require("express");
+var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require("./config/mongoose"); //obteniendo las configuraciones de la base 
-const apiRouter = require('./routes/api/index.router');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-mongoose.connect(); //Luego de inicializar el servicio se hace la conexión a la base de datos
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/api", apiRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
